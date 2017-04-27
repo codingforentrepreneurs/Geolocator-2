e141bc61cdd6c341884cdecdd99e61b26503a8ac
CFE <hello@teamcfe.com>
2017-04-27 11:14:59 -0700

    13 - Default IP Address & Model Manager

--- backend/src/analytics/models.py
+++ backend/src/analytics/models.py
@@ -5,6 +5,29 @@ from .signals import user_logged_in
 from .utils import get_client_city_data, get_client_ip
 
 
+class UserSessionManager(models.Manager):
+    def create_new(self, user, session_key=None, ip_address=None):
+        session_new = self.model()
+        session_new.user = user
+        session_new.session_key = session_key
+        if ip_address is not None:
+            session_new.ip_address = ip_address
+            city_data = get_client_city_data(ip_address)
+            session_new.city_data = city_data
+            try:
+                city = city_data['city']
+            except:
+                city = None
+            session_new.city = city
+            try:
+                country = city_data['country_name']
+            except:
+                country = None
+            session_new.country = country
+            session_new.save()
+            return session_new
+        return None
+
 class UserSession(models.Model):
     user            = models.ForeignKey(settings.AUTH_USER_MODEL)
     session_key     = models.CharField(max_length=60, null=True, blank=True)
@@ -15,24 +38,29 @@ class UserSession(models.Model):
     active          = models.BooleanField(default=True)
     timestamp       = models.DateTimeField(auto_now_add=True)
 
+    objects = UserSessionManager()
+
     def __str__(self):
-        if self.city_data:
-            return str(self.city_data)
+        city = self.city 
+        country = self.country
+        if city and country:
+            return f"{city}, {country}"
+        elif city and not country:
+            return f"{city}"
+        elif country and not city:
+            return f"{country}"
         return self.user.username
+
 
 def user_logged_in_receiver(sender, request, *args, **kwargs):
     user = sender
-    # ip_address 
-    city_data = None
-    # ip_address = get_client_ip(request)
-    ip_address = '72.14.207.99'
-    if ip_address:
-        city_data = get_client_city_data(ip_address)
+    ip_address = get_client_ip(request)
     session_key = request.session.session_key
-    UserSession.objects.create(user=user, 
+    UserSession.objects.create_new(
+                user=user, 
                 session_key=session_key, 
-                ip_address=ip_address, 
-                city_data=city_data)
+                ip_address=ip_address
+                )
 
 
 
--- backend/src/analytics/utils.py
+++ backend/src/analytics/utils.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.contrib.gis.geoip2 import GeoIP2
 
+GEO_DEFAULT_IP = getattr(settings, 'GEO_DEFAULT_IP', '72.14.207.99')
 
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
@@ -7,7 +9,10 @@ def get_client_ip(request):
         ip = x_forwarded_for.split(',')[0]
     else:
         ip = request.META.get('REMOTE_ADDR')
-    return ip
+    ip_address = ip or GEO_DEFAULT_IP
+    if str(ip_address) == '127.0.0.1':
+        ip_address = GEO_DEFAULT_IP
+    return ip_address
 
 
 def get_client_city_data(ip_address):
